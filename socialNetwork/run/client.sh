970f49c525abdf703adbd83ba69a56c76c8d7ec0
Yifan Qiao <yifanqiao@ucla.edu>
2023-03-04 23:20:23 -0800

    chore [client]: add client container into docker-compose; add client running script

---
+++ socialNetwork/run/client.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export ROOT_DIR=$( cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd )
+
+cp ${ROOT_DIR}/build/src/Client/Client ${ROOT_DIR}/services/Client
+echo "docker exec -it socialnetwork-yifan-client-1 /services/Client $1"
+docker exec -it socialnetwork-yifan-client-1 /services/Client $1
+# docker exec -it socialnetwork-yifan-client-1 /bin/bash
