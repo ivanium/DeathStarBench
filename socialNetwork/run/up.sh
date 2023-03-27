66032bcf5599fb182ff74efd577d2109050d928a
Yifan Qiao <yifanqiao@ucla.edu>
2023-03-27 16:17:27 -0700

    chore [run]: avoid using relative path in up.sh

--- socialNetwork/run/up.sh
+++ socialNetwork/run/up.sh
@@ -6,4 +6,4 @@ ROOT_DIR=$( cd -- "$SCRIPT_DIR/.." &> /dev/null && pwd )
 
 ${SCRIPT_DIR}/cp_services.sh
 
-docker-compose -f docker-compose.yml up -d
+docker-compose -f ${SCRIPT_DIR}/../docker-compose.yml up -d
