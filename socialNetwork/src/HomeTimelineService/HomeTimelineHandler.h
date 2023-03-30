45e5d7fd8502d1c093a08e5e4a291a29d0fb296b
Yifan Qiao <yifanqiao@ucla.edu>
2023-03-30 14:57:53 -0700

    feat [HomeTimeline]: integrate Midas

--- socialNetwork/src/HomeTimelineService/HomeTimelineHandler.h
+++ socialNetwork/src/HomeTimelineService/HomeTimelineHandler.h
@@ -14,6 +14,13 @@
 #include "../ThriftClient.h"
 #include "../logger.h"
 #include "../tracing.h"
+#include "../PostUtils.h"
+
+// [Midas]
+#include "cache_manager.hpp"
+#include "sync_kv.hpp"
+#include "time.hpp"
+constexpr static int kNumBuckets = 1 << 20;
 
 using namespace sw::redis;
 namespace social_network {
@@ -39,6 +46,9 @@ class HomeTimelineHandler : public HomeTimelineServiceIf {
   RedisCluster *_redis_cluster_client_pool;
   ClientPool<ThriftClient<PostStorageServiceClient>> *_post_client_pool;
   ClientPool<ThriftClient<SocialGraphServiceClient>> *_social_graph_client_pool;
+  // [Midas]
+  midas::CachePool *_pool;
+  std::unique_ptr<midas::SyncKV<kNumBuckets>> _post_cache;
 };
 
 HomeTimelineHandler::HomeTimelineHandler(
@@ -50,6 +60,17 @@ HomeTimelineHandler::HomeTimelineHandler(
   _redis_cluster_client_pool = nullptr;
   _post_client_pool = post_client_pool;
   _social_graph_client_pool = social_graph_client_pool;
+
+  auto _cmanager = midas::CacheManager::global_cache_manager();
+  if (!_cmanager->create_pool("posts") ||
+      (_pool = _cmanager->get_pool("posts")) == nullptr) {
+    ServiceException se;
+    se.errorCode = ErrorCode::SE_MIDAS_ERROR;
+    se.message = "Failed to create midas cache pool!";
+    throw se;
+  };
+  _pool->update_limit(512ull * 1024 * 1024); // ~512MB
+  _post_cache = std::make_unique<midas::SyncKV<kNumBuckets>>(_pool);
 }
 
 HomeTimelineHandler::HomeTimelineHandler(
@@ -201,23 +222,84 @@ void HomeTimelineHandler::ReadHomeTimeline(
   for (auto &post_id_str : post_ids_str) {
     post_ids.emplace_back(std::stoul(post_id_str));
   }
-
-  auto post_client_wrapper = _post_client_pool->Pop();
-  if (!post_client_wrapper) {
+  std::map<int64_t, Post> return_map;
+  std::set<int64_t> post_ids_not_cached(post_ids.begin(), post_ids.end());
+  if (post_ids_not_cached.size() != post_ids.size()) {
+    LOG(error)<< "Post_ids are duplicated";
     ServiceException se;
-    se.errorCode = ErrorCode::SE_THRIFT_CONN_ERROR;
-    se.message = "Failed to connect to post-storage-service";
+    se.errorCode = ErrorCode::SE_THRIFT_HANDLER_ERROR;
+    se.message = "Post_ids are duplicated";
     throw se;
   }
-  auto post_client = post_client_wrapper->GetClient();
-  try {
-    post_client->ReadPosts(_return, req_id, post_ids, writer_text_map);
-  } catch (...) {
-    _post_client_pool->Remove(post_client_wrapper);
-    LOG(error) << "Failed to read posts from post-storage-service";
-    throw;
+  midas::kv_types::BatchPlug plug;
+  _post_cache->batch_stt(plug);
+  for (auto &post_id : post_ids) {
+    size_t post_len = 0;
+    char *post_store = reinterpret_cast<char *>(
+        _post_cache->bget_single(&post_id, sizeof(post_id), &post_len, plug));
+    if (post_store) {
+      Post new_post;
+      json post_json =
+          json::parse(std::string(post_store, post_store + post_len));
+      json_utils::JsonToPost(post_json, new_post);
+      return_map.insert(std::make_pair(new_post.post_id, new_post));
+      // filter out
+      post_ids_not_cached.erase(new_post.post_id);
+      free(post_store);
+    }
   }
-  _post_client_pool->Keepalive(post_client_wrapper);
+  _post_cache->batch_end(plug);
+
+  if (!post_ids_not_cached.empty()) {
+    auto missed_cycles_stt = midas::Time::get_cycles_stt();
+    auto post_client_wrapper = _post_client_pool->Pop();
+    if (!post_client_wrapper) {
+      ServiceException se;
+      se.errorCode = ErrorCode::SE_THRIFT_CONN_ERROR;
+      se.message = "Failed to connect to post-storage-service";
+      throw se;
+    }
+    auto post_client = post_client_wrapper->GetClient();
+    std::vector<Post> _return_posts;
+    try {
+      std::vector<int64_t> _post_ids(post_ids_not_cached.begin(),
+                                      post_ids_not_cached.end());
+      post_client->ReadPosts(_return_posts, req_id, _post_ids, writer_text_map);
+    } catch (...) {
+      _post_client_pool->Remove(post_client_wrapper);
+      LOG(error) << "Failed to read posts from post-storage-service";
+      throw;
+    }
+    _post_client_pool->Keepalive(post_client_wrapper);
+
+    try { // merge
+      size_t missed_bytes = 0;
+      for (auto &post : _return_posts) {
+        return_map.insert(std::make_pair(post.post_id, post));
+
+        json post_json;
+        json_utils::PostToJson(post, post_json);
+        std::string post_json_str = post_json.dump();
+        int64_t post_id = post.post_id;
+        _post_cache->set(&post_id, sizeof(post_id), post_json_str.c_str(),
+                         post_json_str.length());
+        missed_bytes += post_json_str.length();
+      }
+      auto missed_cycles_end = midas::Time::get_cycles_end();
+      _pool->record_miss_penalty(missed_cycles_end - missed_cycles_stt,
+                                 missed_bytes);
+    } catch (...) {
+      LOG(error) << "Failed to get post from post-storage-service";
+      throw;
+    }
+  }
+  if (return_map.size() < post_ids.size())
+    LOG(error) << "Failed to get all posts!";
+  std::vector<Post> return_vec;
+  for (auto &post_id : post_ids) {
+    return_vec.emplace_back(return_map[post_id]);
+  }
+  _return = return_vec;
   // span->Finish();
 }
 
